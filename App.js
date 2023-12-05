1784a9f5a42a3ecc1715eec5df9f5f24b949e051
gastonig9 <gastonig2020@gmail.com>
2023-12-05 01:40:15 -0300

    Desafio: Lista Optimizada

--- App.js
+++ App.js
@@ -1,24 +1,84 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, FlatList, TouchableOpacity, StyleSheet } from 'react-native';
 
-export default function App() {
+const TodoListScreen = () => {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState('');
+
+  const addTodo = () => {
+    if (newTodo.trim() !== '') {
+      setTodos([...todos, { id: todos.length + 1, text: newTodo, completed: false }]);
+      setNewTodo('');
+    }
+  };
+
+  const deleteTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  const toggleComplete = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+
   return (
-    <>
-      <View style={styles.container}>
-        <Text>Hola, Coder!</Text>
-        <StatusBar style="auto" />
-      </View>
-      <View>
-      </View>
-    </>
+    <View style={styles.container}>
+      <Text style={styles.title}>Lista Optimizada</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Añadir nueva tarea"
+        value={newTodo}
+        onChangeText={(text) => setNewTodo(text)}
+      />
+      <Button title="Añadir" style={styles.buttonAdd} onPress={addTodo} />
+      <FlatList
+        data={todos}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <TouchableOpacity
+            style={[styles.todoItem, { backgroundColor: item.completed ? '#e0e0e0' : 'white' }]}
+            onPress={() => toggleComplete(item.id)}
+          >
+            <Text>{item.text}</Text>
+            <Button title="Eliminar" onPress={() => deleteTodo(item.id)} />
+          </TouchableOpacity>
+        )}
+      />
+    </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    padding: 16,
+    marginVertical: 50
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 16,
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 16,
+    padding: 8,
+  },
+  todoItem: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: 8,
+    marginVertical: 18,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 8,
   },
 });
+
+export default TodoListScreen;
